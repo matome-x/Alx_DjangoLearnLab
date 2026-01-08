c69d2fb058fa423697b0966ef22be7f7652a96d6
Matome Mahlatsi Modiba <geniustoddler@gmail.com>
2026-01-08 05:08:49 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,5 +15,5 @@ def list_books(request):
 # -----------------------------
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/library_details.html'
     context_object_name = 'library'
