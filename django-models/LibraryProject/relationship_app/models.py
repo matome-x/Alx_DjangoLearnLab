1dcd4b71ebad8bb3be970c8752ead82d8473abee
Matome Mahlatsi Modiba <geniustoddler@gmail.com>
2026-01-08 04:37:02 +0200

    Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -29,3 +29,13 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+# relationship_app/models.py
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.PositiveIntegerField(null=True, blank=True)  # optional field
+
+    def __str__(self):
+        return self.title
