bf2fdb755ebcf065b3fdf63e600b276a607eb472
Matome Mahlatsi Modiba <geniustoddler@gmail.com>
2026-01-09 04:16:31 +0200

    veiws

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,3 +13,18 @@ urlpatterns = [
 ]
 
 
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('login/', LoginView.as_view(
+        template_name='relationship_app/login.html'
+    ), name='login'),
+
+    path('logout/', LogoutView.as_view(
+        template_name='relationship_app/logout.html'
+    ), name='logout'),
+
+    path('register/', views.register, name='register'),
+]
